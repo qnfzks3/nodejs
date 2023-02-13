bbb457c1d0b76a4ba7132b1e2f3d441b1c043199
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-02-13 15:06:15 +0900

    다음 영화 - 셀레니엄2

--- crwaling06b.js
+++ crwaling06b.js
@@ -2,6 +2,19 @@
 // https://movie.daum.net/main
 // 순위, 영화제목, 예약율, 평점
 
+
+
+/*selenium
+
+웹브라우저를 이용한 작업들을 자동화할 수 있도록 특수제작된 브라우저
+axios로 스크래핑할 수 없는 동적 데이터를 포함하는 웹 페이지를
+원격 조작이 가능한 웹브라우저를 이용해서 처리
+로그인을 해서 특정정보를 추출하거나, 반복적인 작업을 통해 정보를 추출하거나
+마우스 클릭등 무인으로 프로그램을 조작해야 할 필요가 있는 경우
+브라우져 자동화 프로그램인 '셀레니엄'을 이용하면 처리 가능
+https://www.npmjs.com/package/selenium-webdriver
+    https://www.selenium.dev/documentation/webdriver/getting_started*/
+
 // 사용할 패키지 가져오기 : require(패키지명)
 const axios = require('axios');
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
