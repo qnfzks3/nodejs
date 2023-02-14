add555c2b39cdaf25fc31016891e6baf12025602
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-02-14 17:28:47 +0900

    mariadb zipcode2013

--- mariadb01b.js
+++ mariadb01b.js
@@ -1,46 +1,35 @@
-const mariadb=require('mariadb');
-const dbconfig=require('./dbconfig2.js');
+const mariadb = require('mariadb');
+const dbconfig = require('./dbconfig2.js');
 
+async function main() {
+    const sql = ' select distinct gugun from zipcode2013 ' +
+        //' where sido = ?';
+        ' where sido = :sido';
 
-async function main(){
-    let sql = ' select distinct sido from zipcode2013 where sido= :sido';
-    //'where sido=?'
-    //'where sido= :sido';
+    let conn = null;
+    //let params = ['서울'];
+    let params = {sido: '서울'};
 
-    let conn=null;                                                //마리아 db는 옵션 이런게 필요없어서 안쓴다.
-    //let params=['서울'];
-    let params={sido:'서울'}
+    try {
+        conn = await mariadb.createConnection(dbconfig);
 
+        // let result = await conn.execute(sql, params);
+        let result = await conn.execute(
+            {namedPlaceholders: true, sql: sql}, params);
 
-    try{
-        conn=await mariadb.createConnection(dbconfig);
-
-        //console.log(result);
-
-        //let result = await conn.execute(sql,params);
-        let result = await conn.execute({namedPlaceholders:true, sql:sql},params);
-
-
-        for (let row of result){
+        for(let row of result) {
             console.log(row.gugun);
         }
-
-    }catch (ex){
+    } catch (ex) {
         console.error(ex)
-    }finally {
-        if (conn){
-            try {
-                await conn.close();
-            }catch (ex){}
+    } finally {
+        if (conn) {
+            try { await conn.close(); }
+            catch (ex) { }
         }
     }
-
-
-}
+}; // main
 
 main();
-
-
-
 
 
--- mariadb01c.js
+++ mariadb01c.js
@@ -1,42 +1,29 @@
-const mariadb=require('mariadb');
-const dbconfig=require('./dbconfig2.js');
+const mariadb = require('mariadb');
+const dbconfig = require('./dbconfig2.js');
 
-
-async function main(){
+async function main() {
     const sql = ' select distinct dong from ' +
-        ' zipcode2013 where sido = ? and gugun = ? gugun order by dong ';
+        ' zipcode2013 where sido = ? and gugun = ? order by dong ';
 
+    let conn = null;
+    let params = ['서울', '강남구']
 
-    let conn=null;                                                //마리아 db는 옵션 이런게 필요없어서 안쓴다.
-    
-    let params=['서울','강남구']
-    
-    try{
-        conn=await mariadb.createConnection(dbconfig);
-        let result=await conn.execute(sql,params);
-        //console.log(result);
+    try {
+        conn = await mariadb.createConnection(dbconfig);
 
+        let result = await conn.execute(sql, params);
 
-        for (let row of result){
-            console.log(row.dong)
+        for(let row of result) {
+            console.log(row.dong);
         }
-
-    }catch (ex){
+    } catch (ex) {
         console.error(ex)
-    }finally {
-        if (conn){
-            try {
-                await conn.close();
-            }catch (ex){}
+    } finally {
+        if (conn) {
+            try { await conn.close(); }
+            catch (ex) { }
         }
     }
-
-
-}
-
-main();
-
-
-
+}; // main
 
 
